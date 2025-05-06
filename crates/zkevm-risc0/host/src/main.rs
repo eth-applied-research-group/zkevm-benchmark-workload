a56c3befd247a8fcf44d033833aa1293a52a63ab
Ignacio Hagopian <jsign.uy@gmail.com>
2025-05-06 17:59:34 -0300

    fix risc0
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- crates/zkevm-risc0/host/src/main.rs
+++ crates/zkevm-risc0/host/src/main.rs
@@ -8,20 +8,16 @@ fn main() {
         .with_env_filter(tracing_subscriber::filter::EnvFilter::from_default_env())
         .init();
 
-    // Obtain the default prover.
-    let prover = default_prover();
-
     run_benchmark(
         RISC0GUEST_ELF,
         "risc0",
         |blockchain_corpus: &BlocksAndWitnesses, _elf_data: &'static [u8]| {
+            // Obtain the default prover.
+            let prover = default_prover();
             let mut reports = Vec::new();
             let corpus_name = &blockchain_corpus.name;
-            let num_blocks_in_corpus = blockchain_corpus.blocks_and_witnesses.len();
 
-            for (block_id, client_input) in
-                blockchain_corpus.blocks_and_witnesses.iter().enumerate()
-            {
+            for client_input in &blockchain_corpus.blocks_and_witnesses {
                 let block_number = client_input.block.number;
 
                 let env = ExecutorEnv::builder()
@@ -33,9 +29,7 @@ fn main() {
                     .unwrap();
 
                 // Proof information by proving the specified ELF binary.
-                let prove_info = prover.prove(env, RISC0GUEST_ELF).unwrap();
-                let receipt = prove_info.receipt;
-                let total_num_cycles = receipt.cycles;
+                let _ = prover.prove(env, RISC0GUEST_ELF).unwrap();
 
                 // RISC0 receipt does not provide detailed region cycle counts by default.
                 // We'll use an empty HashMap for region_cycles.
@@ -43,7 +37,7 @@ fn main() {
 
                 let metrics = WorkloadMetrics {
                     name: format!("{}-{}", corpus_name, block_number),
-                    total_num_cycles,
+                    total_num_cycles: 0, // TODO
                     region_cycles,
                 };
                 reports.push(metrics);
