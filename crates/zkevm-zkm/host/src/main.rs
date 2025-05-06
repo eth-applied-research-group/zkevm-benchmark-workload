c0d42af11e902c91452083bba0829e6991ce1a59
Ignacio Hagopian <jsign.uy@gmail.com>
2025-05-06 16:49:53 -0300

    make zkvm-succinct and zkvm-zkm use benchmark-runner

--- crates/zkevm-zkm/host/src/main.rs
+++ crates/zkevm-zkm/host/src/main.rs
@@ -5,7 +5,8 @@ use zkevm_metrics::WorkloadMetrics;
 use zkm_sdk::{ProverClient, ZKMStdin};
 
 use std::collections::HashMap;
-use witness_generator::generate_stateless_witness;
+use witness_generator::BlocksAndWitnesses;
+use zkm_sdk::{ProverClient, ZKMStdin};
 
 /// Path to the compiled MIPS ELF file for the `zkm-guest` crate.
 pub const STATELESS_ELF: &[u8] = include_bytes!(concat!(
@@ -25,55 +26,33 @@ fn main() {
     // Setup the prover client.
     let client = ProverClient::cpu();
 
-    let generated = generate();
-    let num_corpuses = generated.len();
+    run_benchmark(
+        STATELESS_ELF,
+        "zkm",
+        |blockchain_corpus: &BlocksAndWitnesses, elf_path: &'static [u8]| {
+            let mut reports = Vec::new();
+            let name = blockchain_corpus.name.clone();
+            let num_blocks_in_corpus = blockchain_corpus.blocks_and_witnesses.len();
 
-    for (corpus_id, blockchain_corpus) in generated.into_iter().enumerate() {
-        let mut reports = Vec::new();
-        // Iterate each block in the mini blockchain
-        let name = blockchain_corpus.name.clone();
-        let num_blocks_in_corpus = blockchain_corpus.blocks_and_witnesses.len();
+            for client_input in &blockchain_corpus.blocks_and_witnesses {
+                let block_number = client_input.block.number;
+                let mut stdin = ZKMStdin::new();
+                stdin.write(client_input);
+                stdin.write(&blockchain_corpus.network);
 
-        println!("Processing corpus {} / {num_corpuses}", corpus_id + 1);
-        println!("Corpus name: {name}");
-        println!("Num blocks in corpus: {num_blocks_in_corpus}\n");
+                let (_, report) = client.execute(elf_path, stdin).run().unwrap();
 
-        for (block_id, client_input) in blockchain_corpus.blocks_and_witnesses.iter().enumerate() {
-            println!(
-                "   Processing block {} / {num_blocks_in_corpus}",
-                block_id + 1
-            );
+                let total_num_cycles = report.total_instruction_count();
+                let region_cycles: HashMap<_, _> = report.cycle_tracker.into_iter().collect();
 
-            let block_number = client_input.block.number;
-            let mut stdin = ZKMStdin::new();
-            stdin.write(client_input);
-            stdin.write(&blockchain_corpus.network);
-
-            let (_, report) = client.execute(STATELESS_ELF, stdin).run().unwrap();
-
-            let total_num_cycles = report.total_instruction_count();
-            let region_cycles: HashMap<_, _> = report.cycle_tracker.into_iter().collect();
-
-            let metrics = WorkloadMetrics {
-                name: format!("{}-{}", name, block_number),
-                total_num_cycles,
-                region_cycles,
-            };
-            reports.push(metrics);
-        }
-        WorkloadMetrics::to_path(
-            &format!(
-                "{}/{}/{}/{}.json",
-                env!("CARGO_WORKSPACE_DIR"),
-                "zkevm-metrics",
-                "zkm",
-                name
-            ),
-            &reports,
-        )
-        .unwrap();
-        // Print out the reports to std for now
-        // We can prettify it later.
-        dbg!(&reports);
-    }
+                let metrics = WorkloadMetrics {
+                    name: format!("{}-{}", name, block_number),
+                    total_num_cycles,
+                    region_cycles,
+                };
+                reports.push(metrics);
+            }
+            reports
+        },
+    );
 }
