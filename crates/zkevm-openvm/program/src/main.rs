8b8b4db0a22cf2b86d154c896b95a4fed49e8064
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2025-05-17 23:10:35 +0100

    fix opevm guest program

--- crates/zkevm-openvm/program/src/main.rs
+++ crates/zkevm-openvm/program/src/main.rs
@@ -3,15 +3,16 @@ use openvm::io::read;
 extern crate alloc;
 
 use alloc::sync::Arc;
-use reth_stateless::{ClientInput, fork_spec::ForkSpec, validation::stateless_validation};
+use alloy_genesis::Genesis;
+use reth_stateless::{ClientInput, validation::stateless_validation};
 use tracing_subscriber::fmt;
 
 /// Entry point.
 pub fn main() {
     println!("start read_input");
     let input: ClientInput = read();
-    let network: ForkSpec = read();
-    let chain_spec = Arc::new(network.into());
+    let genesis: Genesis = read();
+    let chain_spec = Arc::new(genesis.into());
     println!("end read_input");
 
     println!("start validation");
